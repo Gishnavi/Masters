5c38bcee7514e2eee16b89a6a4aa0c5d4c6656f5
Gishnavi <gishnavikolli13@gmail.com>
2023-01-30 23:00:14 +0530

    Week-3

---
+++ faqs.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var $ =(id) => document.getElementById(id);
+
+var toggle = function(){
+    console.log("called");
+
+    var h2 = this;
+    var div = h2.nextElementSibling;
+
+    if(h2.hasAttribute("class")){
+        h2.removeAttribute("class");
+    }
+    else{
+        h2.setAttribute("class","minus");
+    }
+
+    if(div.hasAttribute("class")){
+        div.removeAttribute("class");
+    }
+    else{
+        div.setAttribute("class","open");
+    }
+}
+
+onload=function(){
+    var faqs= $("faqs");
+    var h2Elements = faqs.getElementsByTagName("h2");
+
+    for (var i=0; i<h2Elements.length; i++){
+        h2Elements[i].onclick = toggle;
+    }
+}
